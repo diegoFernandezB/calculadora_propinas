66f0dc9552777d982aa96fed108f51a27fa5c433
Ego's Air <ego_frndz@egos.local>
2024-12-18 16:18:37 -0300

    a

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header className=" bg-lime-600 py-5">
+        <h1 className=" text-center text-4xl font-black ">Calculadora de Consumo</h1>
+      </header>
+     
+     
+    </>
+  )
+}
+
+export default App
